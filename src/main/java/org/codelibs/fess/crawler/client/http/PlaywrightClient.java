939aca32b226fddd6cb1d0ad43ce158dab30506a
chendingpan <131523427+chendingpan@users.noreply.github.com>
2024-12-27 10:47:53 +0800

    Update PlaywrightClient.java

--- src/main/java/org/codelibs/fess/crawler/client/http/PlaywrightClient.java
+++ src/main/java/org/codelibs/fess/crawler/client/http/PlaywrightClient.java
@@ -67,6 +67,7 @@ import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.Response;
 import com.microsoft.playwright.options.Cookie;
 import com.microsoft.playwright.options.LoadState;
+import com.microsoft.playwright.options.WaitUntilState;
 import com.microsoft.playwright.options.Proxy;
 
 import jakarta.annotation.Resource;
